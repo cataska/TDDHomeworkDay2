3883195ac0ff531f66a6ca19c910883bae3ff6b0
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-18 11:15:52 +0800

    First red light

--- MyHomework2/MyHomework2/UnitTest1.cs
+++ MyHomework2/MyHomework2/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MyHomework2
@@ -7,8 +8,12 @@ namespace MyHomework2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_One_Volume_1_Price_Should_Be_100()
         {
+            var cart = new Cart();
+            cart.add(new Book(volume = 1, price = 100));
+            var expected = 100;
+            Assert.AreEqual(expected, cart.checkout());
         }
     }
 }
