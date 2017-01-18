cf7148cfb3625a3c19cdff5b9eea7007b1941718
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-18 11:08:01 +0800

    Init homework

---
+++ MyHomework2/MyHomework2/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyHomework2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
