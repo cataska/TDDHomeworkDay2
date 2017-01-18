455cff2b5d95f536561a85938fe22d80caaf145f
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-18 12:29:09 +0800

    Third red light

--- MyHomework2/MyHomework2/UnitTest1.cs
+++ MyHomework2/MyHomework2/UnitTest1.cs
@@ -25,5 +25,16 @@ namespace MyHomework2
             var expected = 190;
             Assert.AreEqual(expected, cart.checkout());
         }
+
+        [TestMethod]
+        public void Test_One_Volume_1_One_Volume_2_One_Volume_3_Price_Should_Be_270()
+        {
+            var cart = new Cart();
+            cart.add(new Book() { volume = 1, price = 100 });
+            cart.add(new Book() { volume = 2, price = 100 });
+            cart.add(new Book() { volume = 3, price = 100 });
+            var expected = 270;
+            Assert.AreEqual(expected, cart.checkout());
+        }
     }
 }
